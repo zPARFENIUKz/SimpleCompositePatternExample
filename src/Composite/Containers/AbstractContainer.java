5e6b0fa8bc579f090df3434d28af057ba722a7e6
Gregory <parfeniuk33@gmail.com>
2023-01-05 00:35:35 +0300

    added builder pattern to creating objects

--- src/Composite/Containers/AbstractContainer.java
+++ src/Composite/Containers/AbstractContainer.java
@@ -1,6 +1,7 @@
 package Composite.Containers;
 
 import Composite.HasPrice;
+import Composite.Items.Item;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -8,6 +9,36 @@ import java.util.List;
 
 public abstract class AbstractContainer implements HasPrice {
     private final List<HasPrice> components = new ArrayList<>();
+
+    public static class Builder {
+        private AbstractContainer container;
+
+        public Builder() {
+
+        }
+
+        public Builder withContainer(AbstractContainer container) {
+            if (container == null) throw new NullPointerException("container can't be null");
+            this.container = container;
+            return this;
+        }
+
+        public Builder addItem(Item item) {
+            if (item == null) throw new NullPointerException("item can't be null");
+            container.addComponent(item);
+            return this;
+        }
+
+        public Builder addContainer(AbstractContainer container) {
+            if (container == null) throw new NullPointerException("container can't be null");
+            this.container.addComponent(container);
+            return this;
+        }
+
+        public AbstractContainer build() {
+            return container;
+        }
+    }
 
     public AbstractContainer() {
 
